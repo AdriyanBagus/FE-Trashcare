2f7b3f10596a147f3340a5fb8bac9f95d00dacb8
AdriyanBagus <AdriyanBagus26@gmail.com>
2023-12-14 01:16:46 +0700

    update chatbot

--- src/component/chatbot/ChatBot.js
+++ src/component/chatbot/ChatBot.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './ChatBot.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./ChatBot.css";
 
 const ChatBot = () => {
   const [messages, setMessages] = useState([]);
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
 
   const handleSendMessage = async () => {
-    if (inputText.trim() === '') return;
+    if (inputText.trim() === "") return;
 
     // Menanggapi pesan dari pengguna
     const userMessage = {
       text: inputText,
-      sender: 'user',
+      sender: "user",
     };
-    setMessages([...messages, userMessage]);
+    setMessages((prevMessages) => [...prevMessages, userMessage]);
 
     try {
       // Mengirim pesan pengguna ke backend Flask
-      const response = await axios.post('http://localhost:5000/api/chatbot', {
-        user_message: inputText,
+      const response = await axios.post("http://localhost:5000/api/chatbot", {
+        message: inputText,
       });
 
       // Menambahkan balasan dari chatbot
       const botMessage = {
-        text: response.data.chatbot_response,
-        sender: 'bot',
+        text: response.data.message,
+        sender: "bot",
       };
-      setMessages([...messages, botMessage]);
+      setMessages((prevMessages) => [...prevMessages, botMessage]);
     } catch (error) {
-      console.error('Error sending message to chatbot:', error);
+      console.error("Error sending message to chatbot:", error);
     }
 
     // Membersihkan input
-    setInputText('');
+    setInputText("");
   };
 
   return (
@@ -45,7 +45,12 @@ const ChatBot = () => {
       <div className="chatbot-container">
         <div className="chatbot-messages">
           {messages.map((message, index) => (
-            <div key={index} className={`message ${message.sender}`}>
+            <div
+              key={index}
+              className={`message ${
+                message.sender === "user" ? "user" : "bot"
+              }`}
+            >
               {message.text}
             </div>
           ))}
